f90f33e5febea9d67a9d303f20e3c99df77ece66
artday <admin@artday.com.ua>
2019-10-08 15:49:36 +0300

    admin-panel's web route added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +12,14 @@
 
 Auth::routes(['verify' => true]);
 
+/* Home */
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+})->name('home');
+
+/* Admin Panel */
+Route::namespace('Admin')->prefix('admin')->middleware(['auth', 'role:admin'])->group( function (){
+    Route::get('{path?}', 'AdminController@panel')
+        ->where('path', '.*')
+        ->name('admin.panel');
 });
