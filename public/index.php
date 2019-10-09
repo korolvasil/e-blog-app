659b5e360a8ade3540c3816327e47b8e2227b23d
artday <admin@artday.com.ua>
2019-10-09 21:54:43 +0300

    updated

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/* Redirect if '/public/...' or '.../index.php' in requested url */
+require __DIR__.'/../bootstrap/redirect.php';
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
