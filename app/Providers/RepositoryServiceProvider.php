40205fe5bd1ad0589a9d1cc494c27950cca8347b
artday <admin@artday.com.ua>
2019-10-12 14:57:03 +0300

    post-repository app binding registered

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\PostRepository;
+use App\Repositories\Eloquent\EloquentPostRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(PostRepository::class, EloquentPostRepository::class);
     }
 
     /**
