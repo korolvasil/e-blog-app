cd36915400bead8e70e4bd9a73f097f222cc6271
artday <admin@artday.com.ua>
2019-10-15 16:50:03 +0300

    model's entity assigned with resolveEntity() if method entity() exists in concrete repository implementation and returns model's class

--- app/Repositories/RepositoryAbstract.php
+++ app/Repositories/RepositoryAbstract.php
@@ -6,5 +6,23 @@ use App\Repositories\Contracts\RepositoryInterface;
 
 abstract class RepositoryAbstract implements RepositoryInterface
 {
+    protected $entity;
 
+    public function __construct()
+    {
+        $this->entity = $this->resolveEntity();
+    }
+
+    public function all()
+    {
+        return $this->entity->all();
+    }
+
+    protected function resolveEntity()
+    {
+        if (!method_exists($this, 'entity')) {
+            throw new \Exception('No Model\'s Entity defined');
+        }
+        return app()->make($this->entity());
+    }
 }
