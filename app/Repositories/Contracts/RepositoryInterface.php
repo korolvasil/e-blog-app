9d47007bd0043394a6566fb1267fcc8a04910f03
artday <admin@artday.com.ua>
2019-10-16 01:36:51 +0300

    common repository methods added

--- app/Repositories/Contracts/RepositoryInterface.php
+++ app/Repositories/Contracts/RepositoryInterface.php
@@ -5,6 +5,12 @@ namespace App\Repositories\Contracts;
 interface RepositoryInterface
 {
     public function entity();
-
     public function all();
+    public function find($id);
+    public function findWhere($column, $value);
+    public function findWhereFirst($column, $value);
+    public function paginate($perPage = 10);
+    public function create(array $properties);
+    public function update($id, array $properties);
+    public function delete($id);
 }
--- app/Repositories/RepositoryAbstract.php
+++ app/Repositories/RepositoryAbstract.php
@@ -4,6 +4,7 @@ namespace App\Repositories;
 
 use Exception;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 use App\Repositories\Exceptions\NoEntityDefined;
 use App\Repositories\Contracts\RepositoryInterface;
 
@@ -25,8 +26,43 @@ abstract class RepositoryAbstract implements RepositoryInterface
         return $this->entity->all();
     }
 
+    public function find($id)
+    {
+        return $this->entity->find($id);
+    }
+
+    public function findWhere($column, $value)
+    {
+        return $this->entity->where($column, $value)->get();
+    }
+
+    public function findWhereFirst($column, $value)
+    {
+        return $this->entity->where($column, $value)->first();
+    }
+
+    public function paginate($perPage = 10)
+    {
+        return $this->entity->paginate($perPage);
+    }
+
+    public function create($properties)
+    {
+        return $this->entity->create($properties);
+    }
+
+    public function update($id, array $properties)
+    {
+        return $this->find($id)->update($properties);
+    }
+
+    public function delete($id)
+    {
+        return $this->find($id)->delete();
+    }
+
     /**
-     * @return Model
+     * @return Model|Builder
      * @throws NoEntityDefined
      */
     protected function resolveEntity()
