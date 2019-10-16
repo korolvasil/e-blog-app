e4beb77b56593fa06d90ad8203f9b6474c4caa11
artday <admin@artday.com.ua>
2019-10-16 11:01:27 +0300

    blog posts index view template added

---
+++ resources/views/blog/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.public.app')
+
+@section('content')
+    @foreach($posts as $item)
+        <article class="mb-5">
+            <header>
+                <h4>{{$item->title}}</h4>
+                <small>by {{$item->user->name}}, {{$item->created_at->diffForHumans()}}</small>
+            </header>
+            <section>
+                {{$item->excerpt}}
+            </section>
+            <footer class="mt-2">
+                <a class="btn btn-primary btn-sm" href="">
+                    read more
+                </a>
+            </footer>
+        </article>
+    @endforeach
+@endsection
+
+@section('aside')
+    <h3>SideBar</h3>
+
+@endsection
