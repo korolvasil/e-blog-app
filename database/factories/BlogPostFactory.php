c6b8036b58665b8faa0eec5c5b6e6302bd35c4bf
artday <admin@artday.com.ua>
2019-10-18 15:25:47 +0300

    refactored

--- database/factories/BlogPostFactory.php
+++ database/factories/BlogPostFactory.php
@@ -6,22 +6,16 @@ use App\Models\BlogPost;
 use Faker\Generator as Faker;
 
 $factory->define(BlogPost::class, function (Faker $faker) {
-
-    $title = $faker->sentence(rand(3, 6), true);
-    $text = $faker->realText(rand(1500, 5000));
-    $isPublished = rand(1, 5) > 1;
-    $createdAt = $faker->dateTimeBetween('-3 months', '-2 months');
-
     return [
         'user_id' => 1,
         'category_id' => rand(1, 20),
-        'title' => $title,
+        'title' => $title = $faker->sentence(rand(3, 6), true),
         'slug' => Str::slug($title),
         'excerpt' => $faker->text(rand(50, 100)),
-        'content' => $text,
-        'is_published' => $isPublished,
+        'content' => $faker->realText(rand(1500, 5000)),
+        'is_published' => $isPublished = rand(1, 5) > 1,
         'published_at' => $isPublished ? $faker->dateTimeBetween('-2 months', '-5 days') : null,
-        'created_at' => $createdAt,
+        'created_at' => $createdAt = $faker->dateTimeBetween('-3 months', '-2 months'),
         'updated_at' => $createdAt
     ];
 });
