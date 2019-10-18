7a1cf3c624234f1969a8525df572b1d37c8b3a14
artday <admin@artday.com.ua>
2019-10-18 15:25:27 +0300

    updated

--- database/factories/BlogCategoryFactory.php
+++ database/factories/BlogCategoryFactory.php
@@ -12,7 +12,7 @@ $factory->define(BlogCategory::class, function (Faker $faker) {
         'slug' => Str::slug($name),
         'is_published' => $isPublished = rand(1, 5) > 1,
         'published_at' => $isPublished ? $faker->dateTimeBetween('-2 months', '-5 days') : null,
-        'description' => $faker->realText(rand(10, 35)),
+        'description' => $faker->realText(rand(15, 35)),
         'created_at' => $createdAt = $faker->dateTimeBetween('-3 months', '-2 months'),
         'updated_at' => $createdAt
     ];
