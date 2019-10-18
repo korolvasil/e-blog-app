be100433f275660b6a5c6066bf99a9c21d4016a2
artday <admin@artday.com.ua>
2019-10-18 14:06:35 +0300

    blog category model created

---
+++ app/Models/BlogCategory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Eloquent\HasLive;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class BlogCategory extends Model
+{
+    use SoftDeletes, HasLive;
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'parent_id', 'user_id', 'is_published'
+    ];
+
+    public function parent()
+    {
+        return $this->belongsTo(BlogCategory::class);
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(BlogPost::class, 'category_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
