3842a30766ca80bdbe6d1e2a5ab401dfaa81f999
artday <admin@artday.com.ua>
2019-10-20 17:41:50 +0300

    getters to check models publish status added

--- app/Traits/Eloquent/HasLive.php
+++ app/Traits/Eloquent/HasLive.php
@@ -3,11 +3,22 @@
 namespace App\Traits\Eloquent;
 
 use Illuminate\Database\Eloquent\Builder;
+use phpDocumentor\Reflection\Types\Boolean;
 
 trait HasLive
 {
     public function scopeLive(Builder $builder)
     {
         return $builder->where('is_published', true);
+    }
+
+    public function isLive()
+    {
+        return  !!$this->is_published;
+    }
+
+    public function isNotLive()
+    {
+        return !$this->isLive();
     }
 }
