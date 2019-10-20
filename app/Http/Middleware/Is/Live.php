289f9f1749617b48b3bb43332355b6db5c3a4b8b
artday <admin@artday.com.ua>
2019-10-20 17:44:51 +0300

    live middleware which throws 404 for unpublished  resources created;

---
+++ app/Http/Middleware/Is/Live.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware\Is;
+
+use Closure;
+
+class Live
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $model)
+    {
+        if ($request->{$model}->isNotLive()) {
+            return abort(404);
+        }
+        return $next($request);
+    }
+}
