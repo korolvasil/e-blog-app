ce5608f847a0f5dcdb8638c17a00457248acae16
artday <admin@artday.com.ua>
2019-10-20 18:07:21 +0300

    refactored with using blog.layout and post.card template

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,25 +1,7 @@
-@extends('layouts.public.app')
+@extends('blog.layout')
 
 @section('content')
-    @foreach($posts as $item)
-        <article class="mb-5">
-            <header>
-                <h4>{{$item->title}}</h4>
-                <small>by {{$item->user->login}}, {{$item->created_at->diffForHumans()}}</small>
-            </header>
-            <section>
-                {{$item->excerpt}}
-            </section>
-            <footer class="mt-2">
-                <a class="btn btn-primary btn-sm" href="">
-                    read more
-                </a>
-            </footer>
-        </article>
+    @foreach($posts as $post)
+        @include('blog.partials.post.card')
     @endforeach
-@endsection
-
-@section('aside')
-    <h3>SideBar</h3>
-
 @endsection
