44b11e8ae09a8113ca1ded5c044d9ce4073b79fb
artday <admin@artday.com.ua>
2019-11-01 04:52:15 +0200

    pagination added

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     @foreach($posts as $post)
+        @php
+            /** @var \App\Models\BlogCategory $category */
+            $category =  $post->category
+        @endphp
         @include('blog.partials.post.card')
     @endforeach
+    <div class="container">{{ $posts->links() }}</div>
 @endsection
