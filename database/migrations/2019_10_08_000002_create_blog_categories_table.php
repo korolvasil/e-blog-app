aa5f2c99d74362e45ecb473a67e1e8c947acb3eb
artday <admin@artday.com.ua>
2019-11-01 23:17:24 +0200

    set nested set package's columns instead parent_id with reference

--- database/migrations/2019_10_08_000002_create_blog_categories_table.php
+++ database/migrations/2019_10_08_000002_create_blog_categories_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Kalnoy\Nestedset\NestedSet;
 
 class CreateBlogCategoriesTable extends Migration
 {
@@ -16,16 +17,19 @@ class CreateBlogCategoriesTable extends Migration
         Schema::create('blog_categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('user_id')->unsigned()->nullable();
-            $table->bigInteger('parent_id')->unsigned()->nullable();
+//            $table->bigInteger('parent_id')->unsigned()->nullable();
             $table->string('slug')->unique();
             $table->string('name');
             $table->text('description')->nullable();
             $table->integer('order')->nullable();
+
+            NestedSet::columns($table);
+
             $table->boolean('is_published')->index()->default(false);
             $table->timestamp('published_at')->nullable();
             $table->softDeletes();
             $table->timestamps();
-            $table->foreign('parent_id')->references('id')->on('blog_categories')->onDelete('set null');
+//            $table->foreign('parent_id')->references('id')->on('blog_categories')->onDelete('set null');
         });
     }
 
