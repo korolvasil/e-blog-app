eb8981460e20cc26c4b1202a5466cbf5e6548b76
artday <admin@artday.com.ua>
2019-11-01 04:48:33 +0200

    updated with blog link

--- resources/views/layouts/public/partials/header/main-nav.blade.php
+++ resources/views/layouts/public/partials/header/main-nav.blade.php
@@ -3,7 +3,7 @@
         <ul>
             <li><a href=""><span>Services</span></a></li>
             <li><a href=""><span>Shop</span></a></li>
-            <li><a href=""><span>Blog</span></a></li>
+            <li><a href="{{ route('blog.posts.all') }}"><span>Blog</span></a></li>
             <li><a href=""><span>Works</span></a></li>
             <li><a href=""><span>About us</span></a></li>
         </ul>
