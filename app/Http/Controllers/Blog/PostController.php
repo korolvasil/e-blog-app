11ee7d88ca5af1405133419ee2581313223bc838
artday <admin@artday.com.ua>
2019-11-01 04:59:31 +0200

    admin's actions deleted

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -14,11 +14,6 @@ use App\Repositories\Eloquent\Criteria\LatestFirst;
 
 class PostController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware(['is.live:post']);
-    }
     /**
      * Display a listing of the resource.
      *
@@ -37,27 +32,6 @@ class PostController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  \App\Models\BlogPost  $post
@@ -66,39 +40,5 @@ class PostController extends Controller
     public function show(BlogPost $post)
     {
         dd($post);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\BlogPost  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(BlogPost $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\BlogPost  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, BlogPost $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\BlogPost  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(BlogPost $post)
-    {
-        //
     }
 }
