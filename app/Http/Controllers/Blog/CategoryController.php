10c7ad5e480591ed46b06ae8bade236be8ad15c7
artday <admin@artday.com.ua>
2019-11-01 16:02:57 +0200

    admin's actions deleted

--- app/Http/Controllers/Blog/CategoryController.php
+++ app/Http/Controllers/Blog/CategoryController.php
@@ -34,27 +34,6 @@ class CategoryController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param  \App\Models\Category  $category
@@ -63,39 +42,5 @@ class CategoryController extends Controller
     public function show(BlogCategory $category)
     {
         dd($category);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return Response
-     */
-    public function edit(BlogCategory $category)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  \App\Models\Category  $category
-     * @return Response
-     */
-    public function update(Request $request, BlogCategory $category)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Category  $category
-     * @return Response
-     */
-    public function destroy(BlogCategory $category)
-    {
-        //
     }
 }
