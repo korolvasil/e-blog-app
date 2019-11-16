040e728abaf049aa49ebced660877e2da06d0c70
artday <admin@artday.com.ua>
2019-11-16 05:11:45 +0200

    refactored with blade's @each directive

--- resources/views/blog/category.blade.php
+++ resources/views/blog/category.blade.php
@@ -2,9 +2,7 @@
 
 @section('content')
     @if($posts->count())
-        @foreach($posts as $post)
-            @include('blog.partials.post.card')
-        @endforeach
+        @each('blog.partials.post.card', $posts, 'post')
         <div class="container">{{ $posts->links() }}</div>
     @else
         There is no posts in this category
