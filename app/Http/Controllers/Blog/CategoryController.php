35bd0eea90fcc6b0e88297ba373436ef540a49b9
artday <admin@artday.com.ua>
2019-11-16 04:30:54 +0200

    refactored with nested set usage and remove getting sidebars's data from controller to the Sidebar's View Composer

--- app/Http/Controllers/Blog/CategoryController.php
+++ app/Http/Controllers/Blog/CategoryController.php
@@ -4,16 +4,13 @@ namespace App\Http\Controllers\Blog;
 
 use App\Models\BlogPost;
 use App\Models\BlogCategory;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\Builder;
 
 use App\Repositories\Contracts\BlogCategoryRepository;
 
 use App\Repositories\Eloquent\Criteria\IsLive;
 use App\Repositories\Eloquent\Criteria\EagerLoad;
-use App\Repositories\Eloquent\Criteria\WithCount;
 
 class CategoryController extends Controller
 {
@@ -38,47 +35,17 @@ class CategoryController extends Controller
      * published posts, that belongs to the children published categories.
      *
      * @param BlogCategory $category
-     * @param BlogCategoryRepository $categories
      * @return void
      */
-    public function posts(BlogCategory $category, BlogCategoryRepository $categories)
+    public function posts(BlogCategory $category)
     {
-        $posts = BlogPost::withLive('category', 'category.parent')
-            ->with('user')
-            ->live()
-            ->latest()
-            ->whereHas('category', function (Builder $q) use ($category) {
-                return $q->live()->whereIn('id', $category->children->pluck('id')->merge($category->id));
+        $posts = BlogPost::withLive('category', 'category.ancestors')
+            ->with('user')->live()->latest()
+            ->whereHas('category', function ($q) use ($category) {
+                return $q->live()->whereIn('id', $category->descendants->pluck('id')->merge($category->id));
             })
             ->paginate();
 
-        $categories = $this->sidebarCategories($categories);
-
-        return view('blog.index', compact('posts', 'categories'));
-    }
-
-    /* SideBar Category Module's categories data with posts count */
-    protected function sidebarCategories(BlogCategoryRepository $categories)
-    {
-        /* Using repository */
-        /*$categories = $categories->withCriteria([
-            new IsLive(),
-            new WithCount(['posts' => function ($q) {
-                return $q->live();
-            }])
-        ])->get()->where('posts_count', '<>', 0);*/
-
-        /* Using Eloquent Builder */
-        $categories = BlogCategory::withCount(['posts' => function ($q) { return $q->live(); }])
-            ->live()
-            ->whereHas('posts', function ($q) { return $q->live();})
-            ->get();
-
-        /* We should increase each category's posts_count with theirs children's posts_count */
-        foreach ($categories as $category) {
-            $category->posts_count += $categories->where('parent_id', $category->id)->pluck('posts_count')->sum();
-        }
-
-        return $categories;
+        return view('blog.category', compact('posts'));
     }
 }
