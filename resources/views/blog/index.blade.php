9eeecc5702e977cb7fc09f1f44a171a03ddbdaee
artday <admin@artday.com.ua>
2019-11-16 04:43:23 +0200

    refactored

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -1,12 +1,6 @@
 @extends('blog.layout')
 
 @section('content')
-    @foreach($posts as $post)
-        @php
-            /** @var \App\Models\BlogCategory $category */
-            $category =  $post->category
-        @endphp
-        @include('blog.partials.post.card')
-    @endforeach
+    @each('blog.partials.post.card', $posts, 'post')
     <div class="container">{{ $posts->links() }}</div>
 @endsection
