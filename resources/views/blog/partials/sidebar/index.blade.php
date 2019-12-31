8ea6cd53a78760929adca0d6c8d32e0734636ecc
artday <admin@artday.com.ua>
2019-12-31 06:13:27 +0200

    categories.flat and tags.list modules added to sidebar

--- resources/views/blog/partials/sidebar/index.blade.php
+++ resources/views/blog/partials/sidebar/index.blade.php
@@ -1,3 +1,5 @@
 @section('aside')
     @include('blog.partials.sidebar.modules.categories.tree')
+    @include('blog.partials.sidebar.modules.categories.flat')
+    @include('blog.partials.sidebar.modules.tags.list')
 @endsection
