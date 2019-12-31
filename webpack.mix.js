8aa8c78c187a374d0f2086991977cc1b2d017a6a
artday <admin@artday.com.ua>
2019-12-31 05:57:15 +0200

    updated

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix
     /* Styles */
-    // .sass('resources/sass/main_admin.scss', 'public/css')
+    .sass('resources/sass/main_admin.scss', 'public/css')
     .sass('resources/sass/main_public.scss', 'public/css');
