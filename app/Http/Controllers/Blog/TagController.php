324676f652796e2bb15b4e5a7228fc1cf751e75b
artday <admin@artday.com.ua>
2019-12-31 07:47:53 +0200

    refactored with PublishedHasLive  relations  queries

--- app/Http/Controllers/Blog/TagController.php
+++ app/Http/Controllers/Blog/TagController.php
@@ -11,18 +11,10 @@ class TagController extends Controller
 {
     public function posts(Tag $tag)
     {
-        $posts = BlogPost::withPublished('category', 'category.ancestors', 'tags')
+        $posts = $tag->posts()
             ->with('user')
-            ->whereIn('id', $tag->posts->pluck('id')->all())
-            ->whereDoesntHave('category', function ($q) {
-                return $q->notPublished();
-            })
-            ->whereDoesntHave('category.ancestors', function ($q) {
-                return $q->notPublished();
-            })
-            ->published()
-            ->latest()
-            ->paginate(5);
+            ->withPublished('category', 'category.ancestors', 'tags')
+            ->live()->latest()->paginate(5);
 
         return view('blog.tag', compact('posts'));
     }
