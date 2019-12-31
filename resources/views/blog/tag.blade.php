b2b73093ea439e3b2ef5e268b07cd0fea2d10d0c
artday <admin@artday.com.ua>
2019-12-31 07:53:44 +0200

    posts by tag template created

---
+++ resources/views/blog/tag.blade.php
@@ -0,0 +1,10 @@
+@extends('blog.layout')
+
+@section('content')
+    @if($posts->count())
+        @each('blog.partials.post.card', $posts, 'post')
+        <div class="container">{{ $posts->links() }}</div>
+    @else
+        There is no posts tagged with
+    @endif
+@endsection
