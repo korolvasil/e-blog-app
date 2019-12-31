692e82b1d1c45d03733e8479ec35b0578877b6bf
artday <admin@artday.com.ua>
2019-12-31 06:22:38 +0200

    categories.flat and tags.list modules view composers registered

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use App\Http\View\Composers\BlogTagsModuleComposer;
+use App\Http\View\Composers\BlogCategoriesModuleComposer;
 
 class ViewServiceProvider extends ServiceProvider
 {
@@ -14,9 +16,12 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        View::composer(
-            'blog.partials.sidebar.modules.categories.tree',
-            'App\Http\View\Composers\SidebarBlogCategoriesModuleComposer'
+        View::composer( ['blog.partials.sidebar.modules.categories.tree', 'blog.partials.sidebar.modules.categories.flat'],
+            BlogCategoriesModuleComposer::class
+        );
+
+        View::composer( ['blog.partials.sidebar.modules.tags.list'],
+            BlogTagsModuleComposer::class
         );
     }
 }
