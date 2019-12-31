93a5f5539f8f45fe7ccca223366548622ba158ca
artday <admin@artday.com.ua>
2019-12-31 06:08:38 +0200

    index action deleted as no needed for category; posts action with PublishedHasLive query constraints updated

--- app/Http/Controllers/Blog/CategoryController.php
+++ app/Http/Controllers/Blog/CategoryController.php
@@ -4,32 +4,10 @@ namespace App\Http\Controllers\Blog;
 
 use App\Models\BlogPost;
 use App\Models\BlogCategory;
-use Illuminate\Http\Response;
 use App\Http\Controllers\Controller;
-
-use App\Repositories\Contracts\BlogCategoryRepository;
-
-use App\Repositories\Eloquent\Criteria\IsLive;
-use App\Repositories\Eloquent\Criteria\EagerLoad;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @param BlogCategoryRepository $categories
-     * @return Response
-     */
-    public function index(BlogCategoryRepository $categories)
-    {
-        $categories = $categories->withCriteria([
-            new IsLive(),
-            new EagerLoad(['user', 'posts', 'posts.user'])
-        ])->get();
-
-        return view('blog.category', compact('categories'));
-    }
-
     /**
      * Display the list of published posts which belongs to specific category with
      * published posts, that belongs to the children published categories.
@@ -39,12 +17,10 @@ class CategoryController extends Controller
      */
     public function posts(BlogCategory $category)
     {
-        $posts = BlogPost::withLive('category', 'category.ancestors')
-            ->with('user')->live()->latest()
-            ->whereHas('category', function ($q) use ($category) {
-                return $q->live()->whereIn('id', $category->descendants->pluck('id')->merge($category->id));
-            })
-            ->paginate();
+        $posts = BlogPost::with('user')
+            ->withPublished('category', 'category.ancestors', 'tags')
+            ->whereIn('category_id', $category->descendants->pluck('id')->merge($category->id))
+            ->live()->latest()->paginate();
 
         return view('blog.category', compact('posts'));
     }
