965233a33ca7314c49821f44eb49dcf0a9234cfe
artday <admin@artday.com.ua>
2019-12-31 06:31:00 +0200

    updated with assigment RouteKeyName to slug and posts polymorphic relation added

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+//use App\Traits\Eloquent\HasPublished;
+use App\Traits\Eloquent\DeletePublishedHasLive\HasLive;
+use App\Traits\Eloquent\PublishedLive\PublishedHasLive;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {
-    //
+    use SoftDeletes, PublishedHasLive;
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function posts()
+    {
+        return $this->morphedByMany(BlogPost::class, 'taggable');
+    }
 }
