8a98ecb8172b78d0c4f28f8251860945095b0489
artday <admin@artday.com.ua>
2019-12-31 07:56:56 +0200

    sidebar's blog tag module's template partial added

---
+++ resources/views/blog/partials/sidebar/modules/tags/list.blade.php
@@ -0,0 +1,6 @@
+<section>
+    <h2>Tags</h2>
+    <ul>
+        @each('blog.partials.sidebar.modules.tags.tagInList', $tags, 'tag')
+    </ul>
+</section>
---
+++ resources/views/blog/partials/sidebar/modules/tags/tagInList.blade.php
@@ -0,0 +1,3 @@
+<li>
+    <a href="{{ route('blog.posts.by.tag', $tag) }}">{{$tag->name}} ({{$tag->posts_count}})</a>
+</li>
