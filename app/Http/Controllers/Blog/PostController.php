b03846b92f32294aa87a1293b0b03278501e3b1a
artday <admin@artday.com.ua>
2019-12-31 07:17:38 +0200

    refactored with PublishedHasLive  relations  queries (such as live and published constraints)

--- app/Http/Controllers/Blog/PostController.php
+++ app/Http/Controllers/Blog/PostController.php
@@ -4,35 +4,23 @@ namespace App\Http\Controllers\Blog;
 
 use App\Models\BlogPost;
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\Builder;
 
 class PostController extends Controller
 {
-    /**
+    /*
      * Display a listing of the published posts with categories, which is live.
-     *
-     * @return void
      */
     public function index()
     {
-        $posts = BlogPost::withLive('category', 'category.ancestors')
-            ->with('user')
-            ->whereDoesntHave('category', function (Builder $q) {
-                return $q->notLive();
-            })
-            ->whereDoesntHave('category.ancestors', function (Builder $q) {
-                return $q->notLive();
-            })
-            ->live()->latest()
-            ->paginate();
+        $posts = BlogPost::with('user')
+            ->withPublished('category', 'category.ancestors', 'tags')
+            ->live()->latest()->paginate();
 
         return view('blog.index', compact('posts'));
     }
 
-    /**
+    /*
      * Display the specified resource.
-     *
-     * @param  BlogPost  $post
      */
     public function show(BlogPost $post)
     {
