30d2c0a88018e95e48aae300dbc538dcbcaa2ac0
artday <admin@artday.com.ua>
2019-12-31 06:09:13 +0200

    Tags Seeder added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UsersTableSeeder::class,
             BlogCategoriesTableSeeder::class,
+            TagsTableSeeder::class,
             BlogPostsTableSeeder::class
         ]);
     }
